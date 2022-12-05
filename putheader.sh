a3d48519b03b9e10f9c03759cb99fec1e8a645fd
timurci <timurcakmakoglu@gmail.com>
2022-12-05 18:50:49 +0300

    added putheader.sh

---
+++ putheader.sh
@@ -0,0 +1,22 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    putheader.sh                                       :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: tcakmako <tcakmako@42.fr>                  +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2022/12/05 18:48:08 by tcakmako          #+#    #+#              #
+#    Updated: 2022/12/05 18:48:08 by tcakmako         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+#!/bin/bash
+
+if [ $# -eq 1 ] && [ $1 != "--help" ]; then
+	find $1 ! -path "*.git*" -type f -exec vim +Stdheader +wq {} \;
+elif [ $# -eq 2 ]; then
+	find $1 ! -path "*.git*" -type f -name "*.$2" -exec vim +Stdheader +wq {} \;
+else
+	echo "putheader <path> [[!]<suffix>]"
+fi
+
