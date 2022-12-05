4fffc5814013458ca4a0ceb86e8aa84a0c3d492f
timurci <timurcakmakoglu@gmail.com>
2022-12-05 19:43:36 +0300

    updated putheader.sh

--- putheader.sh
+++ putheader.sh
@@ -6,7 +6,7 @@
 #    By: tcakmako <tcakmako@42.fr>                  +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/12/05 18:48:08 by tcakmako          #+#    #+#              #
-#    Updated: 2022/12/05 18:48:08 by tcakmako         ###   ########.fr        #
+#    Updated: 2022/12/05 19:43:25 by tcakmako         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -16,7 +16,9 @@ if [ $# -eq 1 ] && [ $1 != "--help" ]; then
 	find $1 ! -path "*.git*" -type f -exec vim +Stdheader +wq {} \;
 elif [ $# -eq 2 ]; then
 	find $1 ! -path "*.git*" -type f -name "*.$2" -exec vim +Stdheader +wq {} \;
+elif [ $# -eq 3 ] && [ $2 == "!" ]; then
+	find $1 ! -path "*.git*" -type f ! -name "*.$3" -exec vim +Stdheader +wq {} \;
 else
-	echo "putheader <path> [[!]<suffix>]"
+	echo "putheader.sh <path> [[!] <suffix>]"
 fi
 
